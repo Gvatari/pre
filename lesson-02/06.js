d0f32aa3e548089cb6e528daed3a8febd03412b8
Alexey <lexa7.03.1994@yandex.ru>
2023-10-20 19:28:33 +0700

    101112

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -7,8 +7,8 @@ let passportWithAddress = {
     }
 };
 
-let qwerty = {...passportWithAddress};
+let qwerty = JSON.parse(JSON.stringify(passportWithAddress));
 qwerty.address.city = "Bobryisk";
 
+console.log(passportWithAddress.address.city); 
 console.log(qwerty.address.city);
-console.log(passportWithAddress.address.city);
