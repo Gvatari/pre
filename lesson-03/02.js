38be8727f4463bb79998875bbee9e3a5a51a7749
Alexey <lexa7.03.1994@yandex.ru>
2023-10-27 21:13:18 +0700

    123

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,7 +1,7 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
 const searchOrangeInArray = (array) => {
-    return array.indexOf("апельсин") ? console.log("Ура! нашел") : console.log("Придется поискать в другом магазине…");
+    array.indexOf("апельсин") ? console.log("Ура! нашел") : console.log("Придется поискать в другом магазине...");
 }
 
 searchOrangeInArray(words);
